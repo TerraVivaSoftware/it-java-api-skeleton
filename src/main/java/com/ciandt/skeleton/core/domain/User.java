92aa9ea4f039444167c4393bdba7b078e8236c4f
marcosvidolin <marcosvidolin@gmail.com>
2020-01-06 10:07:41 -0300

    UUID on User

--- src/main/java/com/ciandt/skeleton/core/domain/User.java
+++ src/main/java/com/ciandt/skeleton/core/domain/User.java
@@ -1,11 +1,13 @@
 package com.ciandt.skeleton.core.domain;
 
+import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 /**
  * This class represents a blog's user.
@@ -21,6 +23,10 @@ public class User {
   @Id
   @Column(name = "USER_TX_LOGIN")
   private String login;
+
+  @Type(type="org.hibernate.type.UUIDCharType")
+  @Column(name = "USER_CD_UUID", unique = true, updatable = false)
+  private UUID uuid;
 
   @Column(name = "USER_TX_NAME")
   private String name;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert
 into
     user
-    (user_tx_email, user_tx_name, user_tx_login)
+    (user_tx_email, user_tx_name, user_tx_login, user_cd_uuid)
 values
-    ('soandso@ciandt.com', 'So and So', 'soandso');
+    ('soandso@ciandt.com', 'So and So', 'soandso', '9B780AAD-8343-24E9-E053-0100007FCF57');
