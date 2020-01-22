5be4c2794ced0bf6b1bc381098cd28aadd794e67
marcosvidolin <marcosvidolin@gmail.com>
2020-01-22 08:11:36 -0300

    Controler Advise

--- src/main/java/com/ciandt/skeleton/web/rest/advise/ErrorMessageResource.java
+++ src/main/java/com/ciandt/skeleton/web/rest/advise/ErrorMessageResource.java
@@ -1,17 +1,16 @@
 package com.ciandt.skeleton.web.rest.advise;
 
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 
 @Data
-public class ErrorResource {
+public class ErrorMessageResource {
 
   private int status;
   private String error;
   private String message;
   private String path;
 
-  public ErrorResource(int status, String error, String message, String path) {
+  public ErrorMessageResource(int status, String error, String message, String path) {
     this.status = status;
     this.error = error;
     this.message = message;
--- src/main/java/com/ciandt/skeleton/web/rest/advise/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/ciandt/skeleton/web/rest/advise/RestResponseEntityExceptionHandler.java
@@ -1,15 +1,15 @@
 package com.ciandt.skeleton.web.rest.advise;
 
 import com.ciandt.skeleton.core.exception.BusinessException;
+import com.ciandt.skeleton.core.exception.RemovePostWithCommentException;
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpHeaders;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.ServletWebRequest;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -18,26 +18,34 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  * @author mvidolin
  * @since Jan 21, 2020
  */
+@Slf4j
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-  private ErrorResource getErrorResource(String cause, final Exception ex, final WebRequest request) {
-    HttpServletRequest req = ((ServletWebRequest) request).getRequest();
-    String path = req.getRequestURI();
-    int httpStatusCode = HttpStatus.CONFLICT.value();
-    return new ErrorResource(httpStatusCode, cause, ex.getMessage(), path);
+  private ResponseEntity<?> getStandardErrorHandler(final HttpStatus httpStatus, final Exception ex
+      , final HttpServletRequest request) {
+    ErrorMessageResource errorMessage = new ErrorMessageResource(httpStatus.value()
+        , ex.getClass().getSimpleName(), ex.getMessage(), request.getRequestURI());
+
+    return ResponseEntity.status(httpStatus.value()).body(errorMessage);
   }
 
   /**
    * The 409 (Conflict) status code indicates that the request could not be completed due to a
    * conflict with the current state of the target resource. This code is used in situations where
    * the user might be able to resolve the conflict and resubmit the request.
+   *
+   * @param ex the exception
+   * @param request the given request
+   * @return returns a message with error
    */
-  @ExceptionHandler(value = {BusinessException.class})
-  @ResponseStatus(value = HttpStatus.CONFLICT)
-  protected ResponseEntity<Object> handleConflict(Exception ex, WebRequest request) {
-    ErrorResource error = getErrorResource("Conflict", ex, request);
-    return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.CONFLICT, request);
+  @ExceptionHandler(value = {
+      BusinessException.class,
+      RemovePostWithCommentException.class
+  })
+  protected ResponseEntity<?> handleConflict(BusinessException ex, HttpServletRequest request) {
+    log.warn(ex.getMessage());
+    return this.getStandardErrorHandler(HttpStatus.CONFLICT, ex, request);
   }
 
 }
